f97ca3539fed0c53a1b86e950fc2a0ec729eacc3
Dangelo Arrivillaga <talgidisoluciones@gmail.com>
2022-03-21 14:36:07 -0400

    Fix linters V2

--- modules/booklist.js
+++ modules/booklist.js
@@ -68,12 +68,11 @@ export default class BookList {
       removeBtn.addEventListener('click', () => {
         this.remove(id);
       });
-      divBtn.append(removeBtn);
-  
+      divBtn.append(removeBtn);  
       bookName.textContent = `"${name}" by`;
       bookAuthor.textContent = author;
       div.append(bookName, bookAuthor, divBtn);
       this.bookSection.append(div);
     }
-  }  
+  }
 }
